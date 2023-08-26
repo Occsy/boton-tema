2d6c68d69e931d441f90d83c9d129e7f8af76d7a
Occsy <thulunitasonriente15@gmail.com>
2023-08-26 11:44:44 -0400

    add opacitity night span

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let circle = document.querySelector("#circle");
 let container = document.querySelector(".container");
 let body = document.querySelector("body");
-let day = document.querySelector('#day')
+let day = document.querySelector('#day');
+let night = document.querySelector("#night");
 circle.addEventListener("click", ()=>{
     circle.classList.toggle("whitemode");
     circle.setAttribute("style", "transition: transform 1s ease;");
@@ -9,7 +10,10 @@ circle.addEventListener("click", ()=>{
     body.classList.toggle("back-ground");
     body.setAttribute("style","transition: background 1s ease")
     day.setAttribute("style", "opacity: 100%;");
-    day.setAttribute("style","transition: opacity 1s ease")
+    day.setAttribute("style","transition: opacity 1s ease");
+    night.setAttribute("style", "opacity: 0;");
+    night.setAttribute("style","transition: opacity 0.5s ease");
+
 
 });
 
